0d4d499fd39eee6f6f1a8d33f9ed4ec91453189d
youben11 <ayouben9@gmail.com>
2020-08-03 14:37:45 +0100

    structure for CLI client: next to add functionalities

--- encrypted_evaluation/client/main.py
+++ encrypted_evaluation/client/main.py
@@ -1,7 +1,10 @@
+from typing import List
 import tenseal as ts
 import typer
 from encrypted_evaluation.client import Client
 
+
+VERBOSE = False
 
 app = typer.Typer()
 
@@ -19,8 +22,16 @@ app = typer.Typer()
 # print(f"[+] Result: {result.decrypt()}")
 
 
+def check_power_of_two(value: int):
+    if value & (value - 1) != 0 or value <= 0:
+        raise typer.BadParameter("Only powers of two greater than zero are allowed")
+
+
 @app.command()
-def ping(url: str):
+def ping(
+    url: str = typer.Argument(..., help="base url of the API (e.g. 'http://myapi.com')")
+):
+    """Check if the API at URL is up"""
     client = Client(url)
     is_up = client.ping()
     if is_up:
@@ -30,17 +41,99 @@ def ping(url: str):
 
 
 @app.command()
-def list_models(url: str):
+def list_models(
+    url: str = typer.Argument(
+        ..., help="base url of the API (e.g. 'http://myapi.com')"
+    ),
+    only_names: bool = typer.Option(
+        False, "--only-names", "-n", help="show only the model names"
+    ),
+):
     client = Client(url)
     models = client.list_models()
     print(models)
 
 
 @app.command()
-def model_info(url: str, model_name: str):
+def model_info(
+    url: str = typer.Argument(
+        ..., help="base url of the API (e.g. 'http://myapi.com')"
+    ),
+    model_name: str = typer.Argument(...),
+):
     client = Client(url)
     model_info = client.model_info(model_name)
     print(model_info)
+
+
+@app.command()
+def evaluate(
+    url: str = typer.Argument(
+        ..., help="base url of the API (e.g. 'http://myapi.com')"
+    ),
+    model_name: str = typer.Argument(...),
+    context_file: typer.FileBinaryRead = typer.Argument(..., envvar="TENSEAL_CONTEXT"),
+    input_file: typer.FileBinaryRead = typer.Argument(...),
+    output_file: typer.FileBinaryWrite = typer.Argument(..., mode="xb"),
+):
+    pass
+
+
+@app.command()
+def decrypt(
+    context_file: typer.FileBinaryRead = typer.Argument(..., envvar="TENSEAL_CONTEXT"),
+    output_file: typer.FileBinaryWrite = typer.Argument(...),
+):
+    pass
+
+
+@app.command()
+def encode(
+    input_file: typer.FileBinaryRead = typer.Argument(...),
+    file_type: str = typer.Option(
+        "", "--type", "-t", help="type of the file to encode"
+    ),
+    method: str = typer.Option("", "--method", "-m", help="encoding method to use"),
+):
+    pass
+
+
+@app.command()
+def create_context(
+    file_name: typer.FileBinaryWrite = typer.Argument(...),
+    poly_mod_degree: int = typer.Argument(
+        ..., help="polynomial modulus degree", callback=check_power_of_two
+    ),
+    coeff_mod_bit_sizes: List[int] = typer.Argument(
+        ..., help="bit size of the coeffcients modulus"
+    ),
+    global_scale: float = typer.Option(
+        None, "--scale", min=1, help="scale to use by default for CKKS encoding",
+    ),
+    gen_galois_keys: bool = typer.Option(
+        False, "--gk/--no-gk", "-g/-G", help="generate galois keys"
+    ),
+    gen_relin_keys: bool = typer.Option(
+        True, "--rk/--no-rk", "-r/-R", help="generate relinearization keys"
+    ),
+    save_secret_key: bool = typer.Option(
+        True, "--sk/--no-sk", "-s/-S", help="save the secret key into the context"
+    ),
+):
+    print(coeff_mod_bit_sizes)
+    print(gen_galois_keys)
+    print(
+        f"GRS flag is {int(gen_galois_keys)}{int(gen_relin_keys)}{int(save_secret_key)}"
+    )
+
+
+@app.callback()
+def main(
+    verbose: int = typer.Option(0, "--verbose", "-v", count=True, help="verbose level")
+):
+    """What this CLI is about?"""
+    if verbose > 0:  # might be worth using the level later
+        VERBOSE = True
 
 
 if __name__ == "__main__":
