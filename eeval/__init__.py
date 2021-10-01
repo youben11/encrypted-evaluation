1e392a18da95ea3ef9b5bf4966b4f9a4494e67cb
removedporn <86824510+removedporn@users.noreply.github.com>
2021-10-01 16:52:19 +0800

    Delete eeval directory

--- eeval/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Client/Server framework for machine learning evaluation on encrypted inputs"""
-
-from eeval.client import Client
-from eeval.server import models
--- eeval/client/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Client to make encrypted evaluation with a remote server."""
-
-from eeval.client.client import Client
-
-
-__all__ = ["Client"]
